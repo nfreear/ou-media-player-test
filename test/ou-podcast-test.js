16667ca50b36333e6c0e2e071a9caf19e4968017
Nick Freear <nfreear@yahoo.co.uk>
2015-01-15 23:05:08 +0000

    Bug #2, Fix for podcast rss test [iet:2703841]
    * `doc.should.contain("application/ttml+xml")`

--- test/ou-podcast-test.js
+++ test/ou-podcast-test.js
@@ -22,6 +22,8 @@ describe("Test OU Podcasts ...", function () {
       res.headers["content-type"].should.contain("application/xml");
 
       doc.should.contain('<?xml version="1.0" encoding="UTF-8"?>');
+      doc.should.contain('xmlns:atom="http://www.w3.org/2005/Atom"');
+      doc.should.contain('xmlns:oup="http://podcast.open.ac.uk/2012"');
       doc.should.contain("<rss xmlns:");
       doc.should.contain("<channel>");
 
@@ -29,7 +31,7 @@ describe("Test OU Podcasts ...", function () {
       doc.should.match(/<oup:aspect_ratio>0.\d/); //item
       doc.should.match(/<oup:published_flag>[01]/);
       doc.should.match(/<atom:link [^>]+oup:transcript/);
-      doc.should.contain("application/ttml+--xml");
+      doc.should.contain("application/ttml+xml");
       doc.should.contain("http://purl.org/steeple/course");
 
       delay(done);
