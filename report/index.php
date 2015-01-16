b52dc228b1dccf101e7f52d04a3efb02821b8df5
Nick Freear <nfreear@yahoo.co.uk>
2015-01-16 23:08:25 +0000

    Bug #6, Re-factor `report/index.php` [iet:2703841]

--- report/index.php
+++ report/index.php
@@ -5,18 +5,13 @@
 * @author Nick Freear, 15 January 2015.
 * @copyright © 2015 The Open University.
 */
-#error_reporting( E_ALL );
-#ini_set( 'display_errors', 1 );
-#header( 'Content-Type: text/html; charset=utf-8' );
-header( 'Access-Control-Allow-Origin: *' );
-
 
 #$report_file = '../_out/report.json';
 $report_file = '../_out/report-spec.txt';
 $report_html = 'spec.html';
 
 
-function _parse_spec( $report_file ) {
+function _parse_test_spec( $report_file ) {
   $stats = null;
   $report_spec = file_get_contents( $report_file );
 
@@ -32,49 +27,48 @@ function _parse_spec( $report_file ) {
       'duration' => $m_pass[ 'time' ] * 1000,
       'end' => date( 'c', $fstat[ 'mtime' ]),
     );
+    $stats->test = $stats->passes + $stats->failures;
 
-    if ($stats->failures) {
-      header( 'HTTP/1.1 503 Service Unavailable' );
-    }
-    header( 'X-test-stats: '. json_encode( $stats ));
-    header( 'X-test-file: ' . $report_file );
+    _test_spec_headers( $stats, $report_file );
+
   } else {
     header( 'HTTP/1.1 500 Internal Server Error' );
   }
   return $stats;
 }
 
-
-$stats = _parse_spec( $report_file );
-
-
 function _NOT_IN_USE_parse_json_spec( $report_file ) {
+  $stats = null;
+  $report_json = file_get_contents( $report_file );
+  $report = json_decode( $report_json );
+  if ($report && isset( $report->stats->passes )) {
 
-$report_json = file_get_contents( $report_file );
-$report = json_decode( $report_json );
-if ($report && isset( $report->stats->passes )) {
-  //$n_fail = count($report->failures);
-  $n_fail = $report->stats->failures;
-  $n_pass = $report->stats->passes;
+    _test_spec_headers( $report->stats, $report_file );
 
-  if ($n_fail) {
-    header( 'HTTP/1.1 503 Service Unavailable' );
+  } else {
+    header( 'HTTP/1.1 500 Internal Server Error' );
   }
-  header( 'X-test-failures: '. $n_fail );
-  header( 'X-test-passes: '. $n_pass );
-  header( 'X-test-end: '   . $report->stats->end );
-  header( 'X-test-stats: ' . json_encode( $report->stats ));
-  header( 'X-test-json-file: ' . $report_file );
-}
-else {
-  header( 'HTTP/1.1 500 Internal Server Error' );
-}
-
   return $report_json;
 }
 
+function _test_spec_headers( $stats, $report_file ) {
+  if ($stats->failures) {
+    header( 'HTTP/1.1 503 Service Unavailable' );
+  }
+  header( 'X-test-passes: '. $stats->passes );
+  header( 'X-test-failures: '. $stats->failures );
+  header( 'X-test-end: '   . $stats->end );
+  header( 'X-test-stats: '. json_encode( $stats ));
+  header( 'X-test-file: ' . $report_file );
 
-if ('json' == _get( 'format' ) && isset( $report_json)) {
+  header( 'Access-Control-Allow-Origin: *' );
+}
+
+
+_parse_test_spec( $report_file );
+
+
+if ('json' == _get( 'format' ) && isset( $report_json )) {
   @header( 'Content-Type: text/json; charset=utf-8' );
   echo $report_json;
 }
@@ -82,10 +76,9 @@ else {
   require_once $report_html;
 }
 
+
 function _get($key, $default = null) {
   return isset($_GET[ $key ]) ? $_GET[ $key ] : $default;
 }
 
 #End.
-
-
